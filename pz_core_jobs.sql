3943d24b5dfcf0da682167286c9b42e1d99a4e77
WirzRexTon <syriusrexton@hotmail.com>
2020-11-29 10:49:00 +0100

    Ajouts du sql des jobs --> Tout est regroupé dedans.
    --> SQL FBI
    --> SQL LSPD
    --> SQL Bahamas
    --> SQL Unicorn
    --> SQL Taxi

---
+++ pz_core_jobs.sql
@@ -0,0 +1,167 @@
+-- Police -- 
+
+INSERT INTO `addon_account` (name, label, shared) VALUES
+	('society_police', 'Police', 1)
+;
+
+INSERT INTO `datastore` (name, label, shared) VALUES
+	('society_police', 'Police', 1)
+;
+
+INSERT INTO `addon_inventory` (name, label, shared) VALUES
+	('society_police', 'Police', 1)
+;
+
+INSERT INTO `jobs` (name, label) VALUES
+	('police', 'LSPD')
+;
+
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
+	('police',0,'recruit','Recrue',20,'{}','{}'),
+	('police',1,'officer','Officier',40,'{}','{}'),
+	('police',2,'sergeant','Sergent',60,'{}','{}'),
+	('police',3,'lieutenant','Lieutenant',85,'{}','{}'),
+	('police',4,'boss','Commandant',100,'{}','{}')
+;
+
+-- Taxi -- 
+
+INSERT INTO `addon_account` (name, label, shared) VALUES
+	('society_taxi', 'Taxi', 1)
+;
+
+INSERT INTO `datastore` (name, label, shared) VALUES
+	('society_taxi', 'Taxi', 1)
+;
+
+INSERT INTO `addon_inventory` (name, label, shared) VALUES
+	('society_taxi', 'Taxi', 1)
+;
+
+INSERT INTO `jobs` (name, label) VALUES
+	('taxi', 'Taxi')
+;
+
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
+	('taxi',0,'recrue','Recrue',12, null),
+	('taxi',1,'novice','Novice',24, null),
+	('taxi',2,'experimente','Experimente',36, null),
+	('taxi',3,'uber','Uber',48,  null),
+	('taxi',4,'boss','Patron',0, null)
+;
+
+-- Unicorn 
+
+SET @job_name = 'unicorn';
+SET @society_name = 'society_unicorn';
+SET @job_Name_Caps = 'Unicorn';
+
+
+
+INSERT INTO `addon_account` (name, label, shared) VALUES
+  (@society_name, @job_Name_Caps, 1)
+;
+
+INSERT INTO `addon_inventory` (name, label, shared) VALUES
+  (@society_name, @job_Name_Caps, 1),
+  ('society_unicorn_fridge', 'Unicorn (frigo)', 1)
+;
+
+INSERT INTO `datastore` (name, label, shared) VALUES 
+    (@society_name, @job_Name_Caps, 1)
+;
+
+INSERT INTO `jobs` (name, label, whitelisted) VALUES
+  (@job_name, @job_Name_Caps, 1)
+;
+
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
+  (@job_name, 0, 'barman', 'Barman', 300, '{}', '{}'),
+  (@job_name, 1, 'dancer', 'Danseur', 300, '{}', '{}'),
+  (@job_name, 2, 'viceboss', 'Co-gérant', 500, '{}', '{}'),
+  (@job_name, 3, 'boss', 'Gérant', 600, '{}', '{}')
+;
+
+INSERT INTO `items` (`name`, `label`, `limit`) VALUES  
+    ('jager', 'Jägermeister', 5),
+    ('vodka', 'Vodka', 5),
+    ('rhum', 'Rhum', 5),
+    ('whisky', 'Whisky', 5),
+    ('tequila', 'Tequila', 5),
+    ('martini', 'Martini blanc', 5),
+    ('soda', 'Soda', 5),
+    ('jusfruit', 'Jus de fruits', 5),
+    ('icetea', 'Ice Tea', 5),
+    ('energy', 'Energy Drink', 5),
+    ('drpepper', 'Dr. Pepper', 5),
+    ('limonade', 'Limonade', 5),
+    ('bolcacahuetes', 'Bol de cacahuètes', 5),
+    ('bolnoixcajou', 'Bol de noix de cajou', 5),
+    ('bolpistache', 'Bol de pistaches', 5),
+    ('bolchips', 'Bol de chips', 5),
+    ('saucisson', 'Saucisson', 5),
+    ('grapperaisin', 'Grappe de raisin', 5),
+    ('jagerbomb', 'Jägerbomb', 5),
+    ('golem', 'Golem', 5),
+    ('whiskycoca', 'Whisky-coca', 5),
+    ('vodkaenergy', 'Vodka-energy', 5),
+    ('vodkafruit', 'Vodka-jus de fruits', 5),
+    ('rhumfruit', 'Rhum-jus de fruits', 5),
+    ('teqpaf', "Teq'paf", 5),
+    ('rhumcoca', 'Rhum-coca', 5),
+    ('mojito', 'Mojito', 5),
+    ('ice', 'Glaçon', 5),
+    ('mixapero', 'Mix Apéritif', 3),
+    ('metreshooter', 'Mètre de shooter', 3),
+    ('jagercerbere', 'Jäger Cerbère', 3),
+    ('menthe', 'Feuille de menthe', 10)
+;
+
+-- Bahamas
+
+
+
+SET @job_name = 'bahamas';
+SET @society_name = 'society_bahamas';
+SET @job_Name_Caps = 'Unicorn';
+
+
+
+INSERT INTO `addon_account` (name, label, shared) VALUES
+  (@society_name, @job_Name_Caps, 1)
+;
+
+INSERT INTO `addon_inventory` (name, label, shared) VALUES
+  (@society_name, @job_Name_Caps, 1),
+  ('society_bahamas_fridge', 'Bahamas (frigo)', 1)
+;
+
+INSERT INTO `datastore` (name, label, shared) VALUES 
+    (@society_name, @job_Name_Caps, 1)
+;
+
+INSERT INTO `jobs` (name, label, whitelisted) VALUES
+  (@job_name, @job_Name_Caps, 1)
+;
+
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
+  (@job_name, 0, 'barman', 'Barman', 300, '{}', '{}'),
+  (@job_name, 1, 'dancer', 'Danseur', 300, '{}', '{}'),
+  (@job_name, 2, 'viceboss', 'Co-gérant', 500, '{}', '{}'),
+  (@job_name, 3, 'boss', 'Gérant', 600, '{}', '{}')
+;
+
+
+--  FBI -- 
+
+
+
+INSERT INTO `jobs`(`name`, `label`, `whitelisted`) VALUES
+    ('fbi', 'FBI', 1)
+;
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
+    ('fbi',0,'recruit','Recrue', 1600,'{}','{}')
+    ('fbi',1,'boss','Boss', 1600,'{}','{}')
+;
+
+
