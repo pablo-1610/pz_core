60e826391192c0fc1931ce16b1457340559f9e56
LozeKylian <kylian4509@hotmail.fr>
2020-12-18 12:10:18 +0100

    Débug "en attente de données" Client Menu

--- modules/menu/menus.lua
+++ modules/menu/menus.lua
@@ -836,13 +836,14 @@ Menus = {
                     RageUI.ButtonWithStyle("Prendre la carte d'identité", nil, { RightLabel = "→→" }, closestPlayer ~= -1 and closestDistance <= 3.0, function(_,_,s)
                         if s then
                             identityStats = nil
-                            pzCore.jobs["police"].getIdentity(closestPlayer)
+                            --pzCore.jobs["police"].getIdentity(closestPlayer)
+                            getInformations(closestPlayer)
                         end
                     end, RMenu:Get('police_dynamicmenu', 'police_dynamicmenu_identity'))
                     RageUI.ButtonWithStyle("Fouiller l'individu", nil, { RightLabel = "→→" }, closestPlayer ~= -1 and closestDistance <= 3.0, function(_,_,s)
                         if s then
                             identityStats = nil
-                            pzCore.jobs["police"].getIdentity(closestPlayer)
+                            getInformations(closestPlayer)
                         end
                     end, RMenu:Get('police_dynamicmenu', 'police_dynamicmenu_bs'))
                     RageUI.ButtonWithStyle("Menotter/Démenotter", nil, { RightLabel = "→→" }, closestPlayer ~= -1 and closestDistance <= 3.0, function(_,_,s)
