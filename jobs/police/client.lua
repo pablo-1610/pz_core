e2409cfb18281caddb2b68d16dab2d73519e1596
LozeKylian <66750292+LozeKylian@users.noreply.github.com>
2021-01-30 17:12:31 +0100

    Debug "Error jobMenu for policeJob"

--- jobs/police/client.lua
+++ jobs/police/client.lua
@@ -52,20 +52,6 @@ RMenu:Get('police_dynamicmenu', 'police_dynamicmenu_announce').Closed = function
 
 local policeOpen = false
 
-local function jobChanged()
-
-	if ESX.PlayerData.job.name == "police" or ESX.PlayerData.job.name == "fbi" then
-		isPolice = true
-		inServicePolice = false
-		pzCore.mug("Statut de service",orgByService[ESX.PlayerData.job.name],"Vous êtes actuellement considéré comme étant ~r~hors service~s~. Vous pouvez changer ce statut avec votre menu ~o~[F6]") 
-		jobMenu()
-	else
-		isPolice = false
-		inServicePolice = false
-		menuThread = false 
-	end
-end
-
 local function jobMenu()
 	if menuThread then return end
 	menuThread = true
@@ -83,6 +69,22 @@ local function jobMenu()
 	end)
 	end
 end
+
+
+local function jobChanged()
+
+	if ESX.PlayerData.job.name == "police" or ESX.PlayerData.job.name == "fbi" then
+		isPolice = true
+		inServicePolice = false
+		pzCore.mug("Statut de service",orgByService[ESX.PlayerData.job.name],"Vous êtes actuellement considéré comme étant ~r~hors service~s~. Vous pouvez changer ce statut avec votre menu ~o~[F6]") 
+		jobMenu()
+	else
+		isPolice = false
+		inServicePolice = false
+		menuThread = false 
+	end
+end
+
 
 local function init()
 	menuThread = false
